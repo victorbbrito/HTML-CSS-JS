c2185401c68d49aafba8d2b46893c2e678baee0b
victorbbrito <victorbbrito@hotmail.com>
2023-04-19 16:12:45 -0400

    19 de abril

--- Gradient Generator App/index.js
+++ Gradient Generator App/index.js
@@ -3,24 +3,25 @@ let colorTwo = document.getElementById("color-b");
 let currentDirection = 'to bottom';
 let outputCode = document.getElementById("code");
 
-function setDirection(value, _this){
-    let directions = document.querySelectorAll(".buttons button");
-    for(let i of directions){
+function setDirection(value, _this) {
+    let direcrtions = document.querySelectorAll(".buttons button");
+    for (let i of direcrtions) {
         i.classList.remove("active");
     }
     _this.classList.add("active");
     currentDirection = value;
 }
 
-function generateCode(){
-    outputCode.value = `background-image: linear-gradient(${currentDirection},${colorOne.value},${colorTwo.value})`;
-    document.getElementsByTagName("BODY")[0].style.backgroundImage = `linear-gradient(${currentDirection},${colorOne.value},${colorTwo.value})`;
+function generateCode() {
+    outputCode.value = `background-image: linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
+    document.getElementsByTagName("BODY")[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
 }
 
-function copyText(){
+function copyText() {
     outputCode.select();
     document.execCommand('copy');
     alert("Gradient Copied!");
 }
+
 
 generateCode();
